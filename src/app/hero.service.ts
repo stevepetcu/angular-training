87d9d24010aaa00e0df9df955f1075d67c8a9b71
Stefan-Daniel Petcu <stefandaniel.petcu@thoughtworks.com>
2018-09-21 12:01:37 +0100

    Initial commit.

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+
+import { HEROES } from './mock-heroes';
+
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) {
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.push('List of heroes fetched successfully.');
+    return of(HEROES);
+  }
+}
